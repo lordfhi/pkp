4dcf7f83534e5e53558cbd63487e8d492a474746
lordfhi <luthfi.ramadn020199@gmail.com>
2019-07-22 11:28:25 +0700

    Testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Luthfi
+                    Luthfi Ramadan
                 </div>
 
                 <div class="links">
